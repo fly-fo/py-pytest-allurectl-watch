4288d3a0c20891241271bcd2ed1ce18dde97a266
Egor I <egor.ivanov@pm.me>
2026-04-23 09:29:16 +0200

    conditions for connection settings and project id
    Signed-off-by: Egor I <egor.ivanov@pm.me>

--- runtests.sh
+++ runtests.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 TIME_STAMP=$(date +%Y%m%d-%H%M)
 
-#connection settings
-export ALLURE_TOKEN=$(security find-generic-password -a "$USER" -s "TESTING_ALLURE_TOKEN" -w)
-export ALLURE_ENDPOINT=$(security find-generic-password -a "$USER" -s "TESTING_ALLURE_ENDPOINT" -w)
+if [[ "$(uname)" == "Darwin" ]]; then
+  export ALLURE_TOKEN=$(security find-generic-password -a "$USER" -s "TESTING_ALLURE_TOKEN" -w)
+  export ALLURE_ENDPOINT=$(security find-generic-password -a "$USER" -s "TESTING_ALLURE_ENDPOINT" -w)
+fi
 
-export ALLURE_PROJECT_ID=14832
+export ALLURE_PROJECT_ID=${1:-14832}
+
 export ALLURE_LAUNCH_NAME="$(date "+%Y-%m-%d %H%M%S") local launch"
 export ALLURE_RESULTS=allure-results
 
